33535f15eba38a0267ce25f5b7bc7d97408a16be
ZIYOCHIRO <ziyochiro@gmail.com>
2018-12-02 19:36:39 +0800

    home scene

--- twitter/AppDelegate.swift
+++ twitter/AppDelegate.swift
@@ -17,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // ignore the storyboard and i'll expain in ep 2 why ...
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        let homeController = HomeController(collectionViewLayout: UICollectionViewFlowLayout())
+        window?.rootViewController = UINavigationController(rootViewController: homeController)
         return true
     }
 
